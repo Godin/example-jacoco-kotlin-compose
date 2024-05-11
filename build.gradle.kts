24951e5aa17bee0b88835eff757a7fe096c75dfd
Evgeny Mandrikov <mandrikov@gmail.com>
2024-05-11 23:29:12 +0200

    (WIP) javap

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,3 +26,22 @@ dependencies {
     testImplementation(libs.junit.api)
     testRuntimeOnly(libs.junit.engine)
 }
+
+tasks.register("javap") {
+  doLast {
+    decomp(file("build/classes/kotlin/main/org/example"))
+  }
+}
+
+fun decomp(f: File) {
+  f.walk().forEach {
+    if (it.extension == "class") {
+      val command = listOf("javap", "-c", "-v", "-p", it.path)
+      val outputFile = File(it.parentFile, it.nameWithoutExtension + ".javap.txt")
+      exec {
+        standardOutput = outputFile.outputStream()
+        commandLine(command)
+      }
+    }
+  }
+}
