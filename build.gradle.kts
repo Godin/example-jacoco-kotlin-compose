c70a8891303bf30563dabf2fb2978de2bb1f3e0a
Evgeny Mandrikov <mandrikov@gmail.com>
2024-10-30 09:58:57 +0100

    Add test coverage verification

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,19 @@ jacoco {
     toolVersion = "[0.8.13-SNAPSHOT,)"
 }
 
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                element = "BUNDLE"
+                counter = "INSTRUCTION"
+                value = "MISSEDCOUNT"
+                maximum = "0".toBigDecimal()
+            }
+        }
+    }
+}
+
 repositories {
     maven {
         url = uri("https://oss.sonatype.org/content/repositories/snapshots")
